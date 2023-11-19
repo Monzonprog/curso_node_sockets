9f744698ac5853dd54fb66719d9e704eb59bce2b
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-11-19 21:33:26 +0100

    Configure socket.io

--- models/server.js
+++ models/server.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 class Server {
 
     constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
+        this.app       = express();
+        this.port      = process.env.PORT;
+        this.server    = require('http').createServer(this.app);
+        this.io        = require('socket.io')(this.server);
 
-        this.paths = {
-        }
+        this.paths = {}
 
         // Middlewares
         this.middlewares();
@@ -32,7 +33,7 @@ class Server {
     routes() { }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
